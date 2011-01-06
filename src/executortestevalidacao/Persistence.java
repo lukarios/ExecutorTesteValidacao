5061ec1da30ed75f0e936ae15b0fc0cda65dc6b6
lukarios <lukariosbertin@gmail.com>
2011-01-06 16:52:21 -0200

    adicionando os novos arquivos do projeto ExecutorTesteVAlidacao

---
+++ src/executortestevalidacao/Persistence.java
@@ -0,0 +1,19 @@
+
+package executortestevalidacao;
+
+/*import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+class Persistence {
+   static private EntityManagerFactory emf = null;
+   static private EntityManager em = null;
+
+   static EntityManager getPersistenceManager() throws Exception {
+       if (em == null) {
+           emf = javax.persistence.Persistence.createEntityManagerFactory("TestToolPU");
+           em = emf.createEntityManager();
+       }
+       return em;
+   }
+} // Persistence
+*/
